c5994a55700d3b2a1ab2198f43dbb756b87bad4e
Jownao <50759662+Jownao@users.noreply.github.com>
2021-02-18 18:42:32 -0300

    Create grafo.py
    etapa 1

---
+++ grafo.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+
+G.add_node('v1')
+G.add_node('v2')
+G.add_node('v3')
+G.add_node('v4')
+G.add_node('v5')
+
+G.add_edge('v1','v2')
+G.add_edge('v2','v3')
+G.add_edge('v3','v4')
+G.add_edge('v4','v5')
+G.add_edge('v5','v1')
+G.add_edge('v2','v4')
+
+plt.figure(2)
+nx.draw_networkx(G, pos=nx.spring_layout(G), with_labels=True)
+plt.show()
