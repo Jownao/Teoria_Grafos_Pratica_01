8cbb0ecef2fffb5fb5f7863c317a616a39c8defa
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2021-02-23 16:55:28 -0300

    Create metodos_basicos.py

---
+++ metodos_basicos.py
@@ -0,0 +1,14 @@
+
+class MetodosBasicos():
+
+    def ehRegular():
+        regular = True
+        return regular
+    
+    def ehCompleto():
+        completo = True
+        return completo
+    
+    def ehConexo():
+        conexo = True
+        return conexo
